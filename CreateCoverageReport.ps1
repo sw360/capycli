d4d5ed2368430037ff62fba80a8899dbdcc2d99d
Thomas Graf <tngraf@gmx.de>
2023-05-19 19:59:43 +0200

    chores: have scripts for code coverage

---
+++ CreateCoverageReport.ps1
@@ -0,0 +1,14 @@
+﻿# ------------------------------------------------
+# Run tests and create code coverage report
+
+# SPDX-FileCopyrightText: (c) 2018-2023 Siemens
+# SPDX-License-Identifier: MIT
+# ------------------------------------------------
+
+poetry run coverage run -m pytest
+poetry run coverage report -m --omit "*/site-packages/*.py"
+poetry run coverage html --omit "*/site-packages/*.py"
+
+
+# -----------------------------------
+# -----------------------------------
---
+++ CreateCoverageReport_NoTest.ps1
@@ -0,0 +1,16 @@
+﻿# ------------------------------------------------
+# Run tests and create code coverage report
+
+# SPDX-FileCopyrightText: (c) 2018-2023 Siemens
+# SPDX-License-Identifier: MIT
+# ------------------------------------------------
+
+# 2022-07-15, T. Graf
+
+poetry run coverage run -m pytest
+poetry run coverage report -m --omit "*/site-packages/*.py",*/tests/*
+poetry run coverage html --omit "*/site-packages/*.py",*/tests/*
+
+
+# -----------------------------------
+# -----------------------------------
