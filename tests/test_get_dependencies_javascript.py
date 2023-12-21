ec2f140419a2a6305102dd15b1a26f530d4f94c7
Shaji <sachin.shaji@siemens.com>
2023-12-21 10:58:12 +0530

    feat: add support for npm package-lock.json which has lockfileVersion 3

--- tests/test_get_dependencies_javascript.py
+++ tests/test_get_dependencies_javascript.py
@@ -20,7 +20,9 @@ from tests.test_base import AppArguments, TestBase
 
 class TestGetDependenciesJavascript(TestBase):
     INPUTFILE1 = "package-lock.json"
+    INPUTFILE2 = "package-lock-3.json"
     OUTPUTFILE1 = "output.json"
+    OUTPUTFILE2 = "output-3.json"
 
     def create_package_lock_1(self, filename: str):
         pl = {}
@@ -46,6 +48,34 @@ class TestGetDependenciesJavascript(TestBase):
         dependencies["zone.js"] = dep2
 
         pl["dependencies"] = dependencies
+
+        write_json_to_file(pl, filename)
+
+    def create_package_lock_3(self, filename: str):
+        pl = {}
+        pl["name"] = "APP"
+        pl["version"] = "0.0.3"
+        pl["lockfileVersion"] = 3
+        pl["requires"] = True
+
+        packages = {}
+
+        dep1 = {}
+        dep1["version"] = "3.0.0"
+        dep1["resolved"] = "artifactory/api/npm/npm-all/@angular/router/-/router-3.0.0.tgz"
+        dep1["integrity"] = "sha1-sqd9GUfj/&hfTGwhfiwmGI="
+        dep1["requires"] = {}
+        dep1["dev"] = "true"
+        packages["@angular/router"] = dep1
+
+        dep2 = {}
+        dep2["version"] = "7.8.0"
+        dep2["resolved"] = "artifactory/api/npm/npm-all/rxjs/-/rxjs-7.8.0.tgz"
+        dep2["integrity"] = "sha1-Pl5NoDxgfJ3NkuN901aHoUoUDBY="
+        dep2["requires"] = {}
+        packages["rxjs"] = dep2
+
+        pl["packages"] = packages
 
         write_json_to_file(pl, filename)
 
@@ -117,6 +147,16 @@ class TestGetDependenciesJavascript(TestBase):
 
         self.delete_file("test_package_lock_1.json")
 
+    def test_convert_package_lock_3(self):
+        self.create_package_lock_3("test_package_lock_3.json")
+        sut = capycli.dependencies.javascript.GetJavascriptDependencies()
+        sbom = sut.convert_package_lock("test_package_lock_3.json")
+        print(type(sbom))
+        print(sbom.components)
+        self.assertEqual(1, len(sbom.components))
+
+        self.delete_file("test_package_lock_3.json")
+
     def test_try_find_metadata_simple(self):
         self.create_package_lock_1("test_package_lock_1.json")
         sut = capycli.dependencies.javascript.GetJavascriptDependencies()
@@ -140,6 +180,30 @@ class TestGetDependenciesJavascript(TestBase):
         self.assertEqual("", val)
 
         self.delete_file("test_package_lock_1.json")
+
+    def test_try_find_metadata_simple_3(self):
+        self.create_package_lock_3("test_package_lock_3.json")
+        sut = capycli.dependencies.javascript.GetJavascriptDependencies()
+        sbom = sut.convert_package_lock("test_package_lock_3.json")
+
+        self.assertEqual(1, len(sbom.components))
+        self.assertEqual("rxjs", sbom.components[0].name)
+        self.assertEqual(None, sbom.components[0].description)
+        val = CycloneDxSupport.get_ext_ref_source_url(sbom.components[0])
+        self.assertEqual("", val)
+
+        enhanced = sut.try_find_metadata(
+            sbom,
+            "https://registry.npmjs.org/")
+        # print_json(enhanced)
+
+        self.assertEqual(1, len(enhanced.components))
+        self.assertEqual("rxjs", enhanced.components[0].name)
+        self.assertEqual("Reactive Extensions for modern JavaScript", enhanced.components[0].description)
+        val = CycloneDxSupport.get_ext_ref_source_url(sbom.components[0])
+        self.assertEqual("https://github.com/reactivex/rxjs/archive/refs/tags/7.8.0.zip", val)
+
+        self.delete_file("test_package_lock_3.json")
 
     def test_issue_100(self):
         bom = SbomCreator.create(None, addlicense=True, addprofile=True, addtools=True)
@@ -183,6 +247,36 @@ class TestGetDependenciesJavascript(TestBase):
 
         self.delete_file(self.OUTPUTFILE1)
 
+    def test_get_metadata_source_archive_url_lockfile3(self) -> None:
+        sut = capycli.dependencies.javascript.GetJavascriptDependencies()
+
+        # create argparse command line argument object
+        args = AppArguments()
+        args.command = []
+        args.command.append("getdependencies")
+        args.command.append("javascript")
+        args.inputfile = os.path.join(os.path.dirname(__file__), "fixtures", self.INPUTFILE2)
+        args.outputfile = self.OUTPUTFILE2
+        args.debug = True
+        args.search_meta_data = True
+
+        out = self.capture_stdout(sut.run, args)
+        self.assertTrue(self.INPUTFILE2 in out)
+        self.assertTrue("Writing new SBOM to output-3.json" in out)
+        self.assertTrue("3 components written to file output-3.json" in out)
+
+        sbom = CaPyCliBom.read_sbom(self.OUTPUTFILE2)
+        self.assertIsNotNone(sbom)
+        self.assertEqual(3, len(sbom.components))
+
+        self.assertEqual("rxjs", sbom.components[2].name)
+        self.assertEqual("7.8.1", sbom.components[2].version)
+        val = CycloneDxSupport.get_ext_ref_source_url(sbom.components[1])
+        print(val)
+        self.assertEqual("https://github.com/angular/angular/archive/refs/tags/15.2.9.zip", val)
+
+        self.delete_file(self.OUTPUTFILE2)
+
     def test_real_package_lock(self) -> None:
         sut = capycli.dependencies.javascript.GetJavascriptDependencies()
 
@@ -215,3 +309,33 @@ class TestGetDependenciesJavascript(TestBase):
         self.assertEqual("0.11.4", sbom.components[5].version)
 
         self.delete_file(self.OUTPUTFILE1)
+
+    def test_real_package_lock_3(self) -> None:
+        sut = capycli.dependencies.javascript.GetJavascriptDependencies()
+
+        # create argparse command line argument object
+        args = AppArguments()
+        args.command = []
+        args.command.append("getdependencies")
+        args.command.append("javascript")
+        args.inputfile = os.path.join(os.path.dirname(__file__), "fixtures", self.INPUTFILE2)
+        args.outputfile = self.OUTPUTFILE2
+        args.debug = True
+        args.search_meta_data = True
+
+        out = self.capture_stdout(sut.run, args)
+        self.assertTrue(self.INPUTFILE2 in out)
+        self.assertTrue("Writing new SBOM to output-3.json" in out)
+        self.assertTrue("3 components written to file output-3.json" in out)
+
+        sbom = CaPyCliBom.read_sbom(self.OUTPUTFILE2)
+        self.assertIsNotNone(sbom)
+        self.assertEqual(3, len(sbom.components))
+        self.assertEqual("@angular/animations", sbom.components[0].name)
+        self.assertEqual("15.2.9", sbom.components[0].version)
+        self.assertEqual("@angular/compiler", sbom.components[1].name)
+        self.assertEqual("15.2.9", sbom.components[1].version)
+        self.assertEqual("rxjs", sbom.components[2].name)
+        self.assertEqual("7.8.1", sbom.components[2].version)
+
+        self.delete_file(self.OUTPUTFILE2)
